b456aa50c29aa60b2fd1886f3b507ece1e351818
mucephei <pparodiramirez@gmail.com>
2022-11-07 19:40:51 -0300

    desafio9-mocks_y_normalizr
    Modificaciones:
    1) Se aplica en servidor el nuevo formato (normalizado) de los mensajes
    2) Front-end
      2.0 Se aplica nuevo formato normalizado de mensajes
      2.1 Ahora el timestamp de los mensajes no se envía, solo se muestra al momento de presentar un nuevo mensaje (lo calcula el cliente)
      2.2 La función showMessages() en front-end se re-escala a presentar un solo mensaje y no a un array --> showMessage()
    3) Los mensajes todavía no son normalizados, solo se aplica formato.

--- desafio9-mocks_y_normalizr/db/db.js
+++ desafio9-mocks_y_normalizr/db/db.js
@@ -1,29 +1,30 @@
 import knex from 'knex';
 import database from '../config/config.js';
-// import { schema, normalize, denormalize } from 'normalizr';
+import { schema, normalize, denormalize } from 'normalizr';
 import { v4 as uuidv4 } from 'uuid';
 import { writeFile, readFile } from 'fs/promises';
 
-// const idSchema = new schema.Entity('identifications');
-// const nameSchema = new schema.Entity('names');
-// const lastNameSchema = new schema.Entity('lastNames');
-// const ageSchema = new schema.Entity('ages');
-// const aliasSchema = new schema.Entity('alias');
-// const avatarSchema = new schema.Entity('avatars');
-// const textSchema = new schema.Entity('texts')
-// const authorSchema = new schema.Entity('authors', {
-//     id: idSchema,
-//     name: nameSchema,
-//     lastName: lastNameSchema,
-//     age: ageSchema,
-//     alias: aliasSchema,
-//     avatar: avatarSchema
-// });
-// const messageSchema = new schema.Entity('messages', {
-//     id: idSchema,
-//     author: authorSchema,
-//     text: [textSchema]
-// });
+const idSchema = new schema.Entity('identifications');
+const emailSchema = new schema.Entity('emails');
+const nameSchema = new schema.Entity('names');
+const lastNameSchema = new schema.Entity('lastNames');
+const ageSchema = new schema.Entity('ages');
+const aliasSchema = new schema.Entity('alias');
+const avatarSchema = new schema.Entity('avatars');
+const textSchema = new schema.Entity('texts')
+const authorSchema = new schema.Entity('authors', {
+    email: emailSchema,
+    name: nameSchema,
+    lastName: lastNameSchema,
+    age: ageSchema,
+    alias: aliasSchema,
+    avatar: avatarSchema
+}, { idAttribute: 'email' });
+const messageSchema = new schema.Entity('messages', {
+    id: idSchema,
+    author: authorSchema,
+    text: [textSchema]
+});
 
 class DB {
     static async getProducts() {
@@ -44,9 +45,8 @@ class DB {
         const messages = await this.readMessages();
         const data = {
             id: uuidv4(),
-            email: newMessage.email,
-            time: newMessage.time,
-            message: newMessage.message
+            author: newMessage.author,
+            text: newMessage.text
         }
         messages.push(data);
         await writeFile('./db/json/messages.json', JSON.stringify(messages, null, 2));
--- desafio9-mocks_y_normalizr/public/js/script.js
+++ desafio9-mocks_y_normalizr/public/js/script.js
@@ -7,7 +7,7 @@ const inputThumbnail = document.getElementById('thumbnail');
 const messageList = document.getElementById('list-messages');
 const formMessage = document.getElementById('form-message');
 const inputUserEmail = document.getElementById('user-email');
-const inputUserMessage = document.getElementById('user-message');
+const inputUserText = document.getElementById('user-text');
 
 let template;           // Template para las card-images de la lista de productos. Es captado mediante un fetch a archivo público de servidor.
 let messages = [];      // Arreglo local de mensajes del Centro de Mensajes.
@@ -37,13 +37,15 @@ socket.on('init-products', (data) => {
 socket.on('log-messages', logMessages => {
     messageList.innerHTML = '';
     messages = logMessages;
-    showMessages(logMessages);
+    messages.forEach(m => {
+        showMessage(m);
+    })
 });
 
 // Se recibe nuevo mensaje desde el Centro de Mensajes.
 socket.on('new-message', newMessage => {
     messages.push(newMessage);
-    showMessages(newMessage);
+    showMessage(newMessage);
 })
 
 // Se recibe un nuevo producto agregado a través del formulario. Se agrega a arreglo local y se presenta.
@@ -67,6 +69,24 @@ addProduct.addEventListener('submit', () => {
 // Se emite nuevo mensaje a todos los clientes conectados.
 formMessage.addEventListener('submit', (e) => {
     e.preventDefault();
+    const data = {
+        author: {
+            email: inputUserEmail.value,
+            name: 'name',
+            lastName: 'lastName',
+            age: 'age',
+            alias: 'alias',
+            avatar: 'avatar',
+        },
+        text: inputUserText.value
+    };
+    socket.emit('send-message', data)
+    inputUserText.value = '';
+    inputUserText.focus();
+});
+
+// Función para presentar nuevos mensajes en el Centro de Mensajes, según formato requerido.
+function showMessage(message) {
     const date = new Date();
     const time = {
         YY: (date.getFullYear()).toString().padStart(2, '0'),
@@ -75,26 +95,11 @@ formMessage.addEventListener('submit', (e) => {
         hh: (date.getHours()).toString().padStart(2, '0'),
         mm: (date.getMinutes()).toString().padStart(2, '0'),
         ss: (date.getSeconds()).toString().padStart(2, '0')
-    }
-    const data = {
-        email: inputUserEmail.value,
-        time: `${time.YY}:${time.MM}:${time.DD} ${time.hh}:${time.mm}:${time.ss}`,
-        message: inputUserMessage.value
     };
-    console.log(data.time);
-    socket.emit('send-message', data)
-    inputUserMessage.value = '';
-    inputUserMessage.focus();
-});
-
-// Función para presentar nuevos mensajes en el Centro de Mensajes, según formato requerido.
-function showMessages(messages) {
-    messages.forEach(m => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <b style="color:#6495ED"> ${m.email}</b>
-            <span style="color:#CD7F32">[${m.time}]</span>:
-            <i style="color:#00A36C">${m.message}</i>`;
-        messageList.appendChild(li);
-    });
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <b style="color:#6495ED"> ${message.author.email}</b>
+        <span style="color:#CD7F32">[${time.YY}/${time.MM}/${time.DD} ${time.hh}:${time.mm}:${time.ss}]</span>:
+        <i style="color:#00A36C">${message.text}</i>`;
+    messageList.appendChild(li);
 }
