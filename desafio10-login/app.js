87939f71e9b7610fb40d26ee9659ce698c4a3497
ppr-flowmeet <ppr@flowmeet.com>
2022-11-15 15:43:26 -0300

    desafio10-login
    1) Implementado persistencia de session en mongo atlas

--- desafio10-login/app.js
+++ desafio10-login/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import expressSession from 'express-session';
+import MongoStore from 'connect-mongo'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import index from './routes/index.js';
@@ -9,10 +10,19 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const advancedOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(expressSession({
+  store: MongoStore.create({
+    mongoUrl: 'mongodb+srv://developer:x6JYg18ip3N7gsky@coderhouse.wm4ogqy.mongodb.net/?retryWrites=true&w=majority',
+    mongoOptions: advancedOptions,
+    ttl: 60,
+  }),
   secret: '3biXMV8#m5s7',
   resave: true,
   saveUninitialized: true,
